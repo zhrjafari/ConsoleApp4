6c61d5b78ed5cdfe5a8ad264b18c5626d206b58d
zahrajafari <jafari_zhr@yahoo.com>
2023-04-15 22:11:14 +0330

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace project
+{
+    class test1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter num1 & num2");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            double num2 = Convert.ToInt32(Console.ReadLine());
+            Calc(num1,num2);
+        }//end main
+
+        private static void Calc(double num1, double num2)
+        {
+            double Result = (0.35*num1) + (0.65 * num2);
+            if (Result > 10)
+                Console.WriteLine("pass");
+            else
+                Console.WriteLine("fail");
+        }
+    }
+}
