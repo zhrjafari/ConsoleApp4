9100342525848f3a6b603bd5cda9e40cb1b3311e
zhrjafari <126233930+zhrjafari@users.noreply.github.com>
2023-04-15 22:20:46 +0330

    Update Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -9,8 +9,8 @@ namespace project
         public static void Main()
         {
             Console.WriteLine("enter num1 & num2");
-            double num1 = Convert.Todouble(Console.ReadLine());
-            double num2 = Convert.Todouble(Console.ReadLine());
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
             Calc(num1,num2);
         }//end main
 
